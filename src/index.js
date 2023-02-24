516c7b10d513a5c9369ff956ddf87d3bf08cb105
mrprotocoll <lekanvgbg@gmail.com>
2023-02-24 08:18:36 +0100

    Replaced BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import TodoApp from './component/TodoApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
