7ebf3b74c38b0dc6a8de5c44117865e87f783d13
mrprotocoll <lekanvgbg@gmail.com>
2023-02-24 07:27:44 +0100

    Added Hamburger menu

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,3 +1,14 @@
-const About = () => <div>About content</div>;
+const About = () => (
+  <div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Asperiores rem,
+      delectus tenetur
+      soluta vero voluptates neque! Voluptate quiquam veniam repellat!
+      Animi veniam esse quo quis, labore laboriosam
+      perspiciatis magni.
+    </p>
+  </div>
+);
 
 export default About;
